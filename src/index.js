bc537aeee0591abfc92e536767a45414d5c7299b
chalosalvador <chalosalvador@gmail.com>
2019-01-18 18:02:52 -0500

    Configuracion inicial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import configureStore, { history } from './store';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './styles/index.css';
+import { LocaleProvider } from 'antd';
+import esES from 'antd/lib/locale-provider/es_ES';
+import 'moment/locale/es';
+import { listenAuthState } from './firebase';
+import { startSetLoginState } from './actions/authActions';
+
+
+const store = configureStore(/* provide initial state if any */ );
+
+const target = document.querySelector( '#root' );
+
+listenAuthState( authUser => {
+  if( authUser ) {
+    store.dispatch( startSetLoginState( authUser.uid ) );
+    renderApp();
+  } else {
+    store.dispatch( startSetLoginState( null ) );
+    renderApp();
+  }
+} );
+
+const renderApp = () => render(
+  <Provider store={ store }>
+    <ConnectedRouter history={ history }>
+      <LocaleProvider locale={ esES }>
+        <App />
+      </LocaleProvider>
+    </ConnectedRouter>
+  </Provider>,
+  target
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
